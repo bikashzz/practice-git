9a2cc1b8722229b140d8960568bf1a13956a24fc
bikashzz <bikashyadav6464@gmail.com>
2024-01-11 12:28:40 +0530

    implementation

--- Calceii.java
+++ Calceii.java
@@ -3,3 +3,16 @@ interface Calceii
     public int getSum(int a,int b)
     public int getSum(int a,int b)
 }
+
+class  CalceiiImp implements Calceii
+{
+     public int getSum(int a,int b)
+   {
+     return a+b;
+   }
+
+    public int getDiff(int a,int b)
+  {
+    return a-b;
+  }
+}
